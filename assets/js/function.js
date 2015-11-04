0a7925f312e6a7907ba75c8dac479c75e90de9e8
Pappum <Alexis@MacBook-Pro-de-Alexis.local>
2015-11-04 11:59:27 +0100

    owl-carousel

---
+++ assets/js/function.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+	$("#owl-demo").owlCarousel({
+ 
+		navigation : true,
+		autoPlay: 3000,
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem: true
+ 
+	});
+ 
+});
