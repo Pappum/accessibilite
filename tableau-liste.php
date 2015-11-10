b8063e24950c1b9592841ddb7050222de7d323c2
Antoine W <antoine-waag@hotmail.fr>
2015-11-10 11:33:59 +0100

    Ajout header/footer

--- tableau-liste.php
+++ tableau-liste.php
@@ -2,42 +2,85 @@
 <html lang="fr">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title>Accueil | Web Agency</title>
+		<title>Web Agency | Agence web à Haguenau</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<!-- Styles -->
-		<link rel="stylesheet" href="assets/css/knacss.css">
-		<link rel="stylesheet" href="assets/css/style.css">
 		
+		<!-- Owl Carousel -->
+		<link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css">
+		<link rel="stylesheet" href="assets/owl-carousel/owl.theme.css">
+		<link rel="stylesheet" href="assets/owl-carousel/owl.transitions.css">
+		
+		<!-- Styles -->
+		<link rel="stylesheet" href="assets/css/knacss-unminified.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+
 	</head>
 	<body>
+	
+	<?php 
+        // Init variable tabindex
+        $i = 0;
+    ?>
 		<header role="banner">
-			<nav role="navigation" aria-label="Menu principal" class="wrapper">
-				<ul id="evitement">
-				   <li><a href="#contenu">Aller au contenu</a></li>
-				   <li><a href="#menu">Aller au menu</a></li> 
-				   <li><a href="#recherche">Aller à la recherche</a></li>
-				</ul>
+           <div class="wrapper">
+                
+                <!-- NE MARCHE PAS 
+                <ul id="evitement">
+                   <li><a id="test" href="#contenu" tabindex="1" accesskey="c">Aller au contenu</a></li>
+                   <li><a href="#menu" tabindex="2" accesskey="m">Aller au menu</a></li> 
+                   <li><a href="#recherche" tabindex="3" accesskey="r">Aller à la recherche</a></li>
+                </ul> -->
+                
+                <!-- Autre solution -->
+                <div id="evitement">
+                    <button onclick="window.location.hash='#owl-demo'" tabindex="<?php echo $i += 1; ?>" accesskey="c">Aller au contenu</button>
+                    <button onclick="window.location.hash='#menu'" tabindex="<?php echo $i += 1; ?>" accesskey="m">Aller au menu</button>
+                    <button onclick="window.location.hash='#recherche'" tabindex="<?php echo $i += 1; ?>" accesskey="r">Aller à la recherche</button>
+                </div>
+                
+                
+                <h1>Web Agency</h1>
+                <nav id="navigation" role="navigation" aria-label="Menu principal">
+                    <ul role="menubar" aria-hidden="false" class="nav-menu" id="menu">
+                        <li role="menuitem"><a href="index.php" title="Accueil" tabindex="<?php echo $i += 1; ?>">Accueil</a></li>
 
-				<ul id="menu">
-                    <li><a href="#">Accueil</a></li>
-                    <li><a href="video.html">Vidéo</a></li>
-                    <li><a href="son.html">Son</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-				</ul>
+                        <li role="menuitem" aria-haspopup="true" class="nav-item">
+                            <a href="#" title="Fonctionnalités" tabindex="<?php echo $i += 1; ?>">Fonctionnalités</a>
+                            <div class="sub-nav">
+                                <ul class="sub-nav-group" aria-hidden="true">
+                                    <li><a href="video.php" title="Vidéo" tabindex="<?php echo $i += 1; ?>">Vidéo</a></li>
+                                    <li><a href="calendrier.php" title="Calendrier" tabindex="<?php echo $i += 1; ?>">Calendrier</a></li>
+                                    <li><a href="tableau.php" title="Tableau" tabindex="<?php echo $i += 1; ?>">Tableau</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        
+                        <li role="menuitem" aria-haspopup="true" class="nav-item">
+                            <a href="#" title="Nos services" tabindex="<?php echo $i += 1; ?>">Nos services</a>
+                            <div class="sub-nav">
+                                <ul class="sub-nav-group" aria-hidden="true">
+                                    <li><a href="#" title="Site internet" tabindex="<?php echo $i += 1; ?>">Site internet</a></li>
+                                    <li><a href="#" title="Webdesign" tabindex="<?php echo $i += 1; ?>">Webdesign</a></li>
+                                    <li><a href="#" title="Solution mobile" tabindex="<?php echo $i += 1; ?>">Solution mobile</a></li>
+                                </ul>
+                            </div>
+                        </li>
 
-				<div id="recherche" role="search">
-                    <form>
-                        <input type="text" title="Recherche par mots-clés" />
-                        <input type="submit" value="Rechercher" />
-                    </form>
-				</div>
-			</nav>
+                        <li role="menuitem"><a href="contact.php" title="Contact" tabindex="<?php echo $i += 1; ?>">Contact</a></li>
+                    </ul>
+
+                    <div id="recherche" role="search">
+                        <form>
+                            <input tabindex="<?php echo $i += 1; ?>" type="text" placeholder="Rechercher" title="Recherche par mots-clés" />
+                            <input tabindex="<?php echo $i += 1; ?>" type="submit" value="Ok" />
+                        </form>
+                    </div>
+                </nav>
+            </div>
 		</header>
 
 		<main class="wrapper" id="contenu" role="main">
-			<!-- <nav role="navigation" aria-label="Fil d'Ariane"></nav> -->
             <section>
                 <p id="liste">Part du marché des navigateurs internet</p>
                 
@@ -65,11 +108,30 @@
 
 		<footer role="contentinfo">
             <div class="wrapper">
-				<ul>
-	                <li><a href="apropos.html"></a>A propos</li>
-	                <li><a href="mentions.html"></a>Mentions légales</li>
-				</ul>
-				<p>© Web Agency</p>
+            	<div class="grid-4-small-1">
+            		<div>
+            			<ul>
+                            <span>Menu</span>
+                            <li><a href="#" title="Fonctionnalités" tabindex="<?php echo $i += 1; ?>">Fonctionnalités</a></li>
+                            <li><a href="#" title="Nos services" tabindex="<?php echo $i += 1; ?>">Nos services</a></li>
+                            <li><a href="#" title="Contact" tabindex="<?php echo $i += 1; ?>">Contact</a></li>
+						</ul>
+            		</div>
+            		<div>
+            		    <span>Contact</span>
+            		    <p>03 88 36 20 43<br>2 rue de Strasbourg<br>67500 Haguenau</p>
+            		</div>
+            		<div>
+            		    <span>Réseaux sociaux</span>
+            		    <p>
+            		        <a href="http://www.facebook.com" title="Lien page Facebook" tabindex="<?php echo $i += 1; ?>"><img src="assets/images/fb.png" alt="Logo Facebook"></a>
+            		        <a href="http://www.twitter.com" title="Lien page Twitter" tabindex="<?php echo $i += 1; ?>"><img src="assets/images/tw.png" alt="Logo Twitter"></a>
+            		    </p>
+            		</div>
+            		<div>
+            			<p>© Web Agency - <a href="#" title="Mentions légales">Mentions légales</a></p>
+            		</div>
+            	</div>
             </div>
 		</footer>
 
--- tableau.php
+++ tableau.php
@@ -2,10 +2,10 @@
 <html lang="fr">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title>Accueil | Web Agency</title>
+		<title>Web Agency | Agence web à Haguenau</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
+		
 		<!-- Owl Carousel -->
 		<link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css">
 		<link rel="stylesheet" href="assets/owl-carousel/owl.theme.css">
@@ -14,45 +14,70 @@
 		<!-- Styles -->
 		<link rel="stylesheet" href="assets/css/knacss-unminified.css">
 		<link rel="stylesheet" href="assets/css/style.css">
-		
+
 	</head>
 	<body>
-
+	
+	<?php 
+        // Init variable tabindex
+        $i = 0;
+    ?>
 		<header role="banner">
-			<ul id="evitement">
-			   <li><a href="#contenu" tabindex="1">Aller au contenu</a></li>
-			   <li><a href="#menu" tabindex="2">Aller au menu</a></li> 
-			   <li><a href="#recherche" tabindex="3">Aller à la recherche</a></li>
-			</ul>
-			<nav id="navigation" role="navigation" aria-label="Menu principal" class="wrapper">
-				<ul role="menubar" aria-hidden="false" class="nav-menu" id="menu">
-					<li role="menuitem"><a href="index.html" title="Accueil (rubrique courante)" tabindex="4">Accueil</a></li>
-					
-					<li role="menuitem"><a href="about.html" title="" tabindex="5" class="active">A propos</a></li>
-					
-					<li role="menuitem" aria-haspopup="true" class="nav-item">
-						<a href="services.html" title="Nos services" tabindex="6">Nos services</a>
-						<div class="sub-nav">
-							<ul class="sub-nav-group" aria-hidden="true">
-								<li><a href="#">Site internet</a></li>
-								<li><a href="#">Webdesign</a></li>
-								<li><a href="#">Solution mobile</a></li>
-							</ul>
-						</div>
-					</li>
+           <div class="wrapper">
+                
+                <!-- NE MARCHE PAS 
+                <ul id="evitement">
+                   <li><a id="test" href="#contenu" tabindex="1" accesskey="c">Aller au contenu</a></li>
+                   <li><a href="#menu" tabindex="2" accesskey="m">Aller au menu</a></li> 
+                   <li><a href="#recherche" tabindex="3" accesskey="r">Aller à la recherche</a></li>
+                </ul> -->
+                
+                <!-- Autre solution -->
+                <div id="evitement">
+                    <button onclick="window.location.hash='#owl-demo'" tabindex="<?php echo $i += 1; ?>" accesskey="c">Aller au contenu</button>
+                    <button onclick="window.location.hash='#menu'" tabindex="<?php echo $i += 1; ?>" accesskey="m">Aller au menu</button>
+                    <button onclick="window.location.hash='#recherche'" tabindex="<?php echo $i += 1; ?>" accesskey="r">Aller à la recherche</button>
+                </div>
+                
+                
+                <h1>Web Agency</h1>
+                <nav id="navigation" role="navigation" aria-label="Menu principal">
+                    <ul role="menubar" aria-hidden="false" class="nav-menu" id="menu">
+                        <li role="menuitem"><a href="index.php" title="Accueil" tabindex="<?php echo $i += 1; ?>">Accueil</a></li>
 
-					<li role="menuitem"><a href="son.html" title="" tabindex="7">Son</a></li>
-					
-					<li role="menuitem"><a href="contact.html" title="Contact" tabindex="8">Contact</a></li>
-				</ul>
+                        <li role="menuitem" aria-haspopup="true" class="nav-item">
+                            <a href="#" class="active" title="Fonctionnalités" tabindex="<?php echo $i += 1; ?>">Fonctionnalités</a>
+                            <div class="sub-nav">
+                                <ul class="sub-nav-group" aria-hidden="true">
+                                    <li><a href="video.php" title="Vidéo" tabindex="<?php echo $i += 1; ?>">Vidéo</a></li>
+                                    <li><a href="calendrier.php" title="Calendrier" tabindex="<?php echo $i += 1; ?>">Calendrier</a></li>
+                                    <li><a href="#" class="active" title="Tableau (rubrique courante)" tabindex="<?php echo $i += 1; ?>">Tableau</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        
+                        <li role="menuitem" aria-haspopup="true" class="nav-item">
+                            <a href="#" title="Nos services" tabindex="<?php echo $i += 1; ?>">Nos services</a>
+                            <div class="sub-nav">
+                                <ul class="sub-nav-group" aria-hidden="true">
+                                    <li><a href="#" title="Site internet" tabindex="<?php echo $i += 1; ?>">Site internet</a></li>
+                                    <li><a href="#" title="Webdesign" tabindex="<?php echo $i += 1; ?>">Webdesign</a></li>
+                                    <li><a href="#" title="Solution mobile" tabindex="<?php echo $i += 1; ?>">Solution mobile</a></li>
+                                </ul>
+                            </div>
+                        </li>
 
-				<div id="recherche" role="search">
-					<form>
-						<input type="text" placeholder="Rechercher" title="Recherche par mots-clés" />
-						<input type="submit" value="Ok" />
-					</form>
-				</div>
-			</nav>
+                        <li role="menuitem"><a href="contact.php" title="Contact" tabindex="<?php echo $i += 1; ?>">Contact</a></li>
+                    </ul>
+
+                    <div id="recherche" role="search">
+                        <form>
+                            <input tabindex="<?php echo $i += 1; ?>" type="text" placeholder="Rechercher" title="Recherche par mots-clés" />
+                            <input tabindex="<?php echo $i += 1; ?>" type="submit" value="Ok" />
+                        </form>
+                    </div>
+                </nav>
+            </div>
 		</header>
 
 		<main id="contenu" role="main">
@@ -130,15 +155,32 @@
 		</main>
 
 		<footer role="contentinfo">
-			<div class="wrapper">
-				<ul>
-					<li><a href="apropos.html"></a>A propos</li>
-					<li><a href="mentions.html"></a>Mentions légales</li>
-				</ul>
-				<p>© Web Agency</p>
-			</div>
-
-			<div id="results"></div>
+            <div class="wrapper">
+            	<div class="grid-4-small-1">
+            		<div>
+            			<ul>
+                            <span>Menu</span>
+                            <li><a href="#" title="Fonctionnalités" tabindex="<?php echo $i += 1; ?>">Fonctionnalités</a></li>
+                            <li><a href="#" title="Nos services" tabindex="<?php echo $i += 1; ?>">Nos services</a></li>
+                            <li><a href="#" title="Contact" tabindex="<?php echo $i += 1; ?>">Contact</a></li>
+						</ul>
+            		</div>
+            		<div>
+            		    <span>Contact</span>
+            		    <p>03 88 36 20 43<br>2 rue de Strasbourg<br>67500 Haguenau</p>
+            		</div>
+            		<div>
+            		    <span>Réseaux sociaux</span>
+            		    <p>
+            		        <a href="http://www.facebook.com" title="Lien page Facebook" tabindex="<?php echo $i += 1; ?>"><img src="assets/images/fb.png" alt="Logo Facebook"></a>
+            		        <a href="http://www.twitter.com" title="Lien page Twitter" tabindex="<?php echo $i += 1; ?>"><img src="assets/images/tw.png" alt="Logo Twitter"></a>
+            		    </p>
+            		</div>
+            		<div>
+            			<p>© Web Agency - <a href="#" title="Mentions légales">Mentions légales</a></p>
+            		</div>
+            	</div>
+            </div>
 		</footer>
 
 
